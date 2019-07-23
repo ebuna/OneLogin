2c88edb0e647df269f05854846d3aa8bc1b5a20c
Elie Bunassar <ebuna@Elies-MacBook-Pro.local>
2019-07-23 15:12:51 -0400

    initial commit

---
+++ OneLogin.psm1
@@ -0,0 +1,6 @@
+# Make all errors terminating for try/catch exception handling
+$ErrorActionPreference = "Stop"
+
+# Static variables
+Set-Variable -Name "baseURI" -Value "https://api.us.onelogin.com/api/1"
+
