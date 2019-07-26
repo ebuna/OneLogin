2c6511ffd09d775a2d88612533c37d962bdaf85f
Elie Bunassar <ebuna@Elies-MacBook-Pro.local>
2019-07-25 22:26:05 -0400

    added GetAllPaginationData

--- OneLogin.psm1
+++ OneLogin.psm1
@@ -209,6 +209,43 @@ Function Get-OLEvents {
 }
 
 
+
+
+Function GetAllPaginationData {
+
+	[CmdletBinding()]
+    Param (
+
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName,
+        ValueFromPipeline,
+        Position = 0)]
+        [String]
+        $Headers,
+
+        [Parameter(Mandatory = $false,
+        ValueFromPipelineByPropertyName,
+        ValueFromPipeline,
+        Position = 1)]
+        [int]
+        $Response
+    )
+	
+	$tempData = $Response
+	$allData = $Response.data
+	
+	while ($tempData.pagination.next_link) {
+	
+		$tmpIRM = Invoke-RestMethod -URI $tempData.pagination.next_link -Method Get -Headers $Headers
+		
+		$allData += $tmpIRM.data
+		$tempData = $tmpIRM
+	}
+	
+	return $allData
+}
+
+
 Function ValidateToken {
 
     # Check for existing access token and use or refresh it
